86b3471c1ca4388f3d7b2d446f90875689c20e00
Tony <torretony94@gmail.com>
2023-05-11 23:50:20 +0200

    finito per stasera!

--- progetto-Agenzia-Viaggi/routes/web.php
+++ progetto-Agenzia-Viaggi/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AirController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AirController::class , 'welcome'])->name('welcome');
+
+Route::get('/detail/{destinazione}', [AirController::class , 'detail'])->name('detail');
